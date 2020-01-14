8cf59e91146d34efd5452c71d2de3c92f335fc21
mjwills-inf <mjwills@live.co.uk>
2020-01-14 22:46:15 +0000

    pseudo

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const gameBoard = (function() {
   
-  const boardTiles = ["", "", "", "", "", "", "", "", ""]
+  const boardTiles = [
+    ["", "", ""]
+    ["", "", ""]
+    ["", "", ""]
+  ];
+
+  // render inside
+  // -1, 0, 1 ()
+
 
   
 
@@ -10,7 +18,25 @@ const players = () => {
 
 };
 
+
+
 const game = (function() {
 
 
 })();
+
+// variable for game StaticRange, running, turn
+// event listeners 
+
+// 2 players, gameboard, game classes
+// in game have reference to players and game
+// have reference to state (running, results, over)
+// current player variable
+// function swtich player
+// event listeners
+// on click grab current player from game object 
+// assign to grid
+// check if complete
+// switch player if so
+
+// render function in game that calls gameboard.render 
