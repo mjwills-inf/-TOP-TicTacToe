fd77a6d90734f714c53103f3acd964c914e565d9
mjwills-inf <mjwills@live.co.uk>
2020-01-16 21:22:13 +0000

    rough game and render functions complete

--- script.js
+++ script.js
@@ -1,84 +1,139 @@
-/////////////////////////////////////////////////////////////////////
-const gameBoard = (function() {
-  
-  const gridContainer = document.getElementById('grid-container');
-  console.log(gridContainer);
-
-  
+const gameBoard = (function() {   
   let boardTiles = [
-    [1, 2, 2],
-    [1, 2, 1],
-    [2, 1, 0],
+    [0, 0, 0],    
+    [0, 0, 0],
+    [0, 0, 0],
   ];
+  const gridContainer = document.getElementById('grid-container');  
+  const getBoard = () => boardTiles
 
   const renderBoard = () => {
     for (i=0; i<boardTiles.length; i++) {
       for (j=0; j<boardTiles[i].length; j++) {
         let newDiv = document.createElement('div');
         newDiv.classList = 'tile';
-        newDiv.setAttribute('array-ref', i + "" + j);
+        newDiv.setAttribute('id', i + "" + j);
         newDiv.addEventListener('click', game.tileSelection);       
         gridContainer.appendChild(newDiv);        
       }
     }
-  }
-
+  }  
   const renderMark = (whichTile, whichMark) => {
-    // split array ref into 2 variables and use those variables in bracket note for array
-    // boardTiles[var1][var2]
-    
-    //.setAttribute('content', boardTiles[i][j])
-    
-    console.log(whichTile);
-    console.log(typeof(whichTile));
-  }   
-
+    let arrayRefOne = whichTile.charAt(0);
+    let arrayRefTwo = whichTile.charAt(1);
+    let targetDiv = document.getElementById(whichTile);
+    targetDiv.setAttribute('content', whichMark);
+    boardTiles[arrayRefOne][arrayRefTwo] = whichMark;  
+  }
   return {
     renderBoard,
     renderMark,
-  }
-  
+    getBoard,
+  }  
 })();
 
 
-//////////////////////////////////////////////////////////////////////
-const players = () => {
+const players = (name, pNum) => {
+  const getName = () => name;
+  const mark = () => pNum;
+  return {getName, mark}  
 };
 
+//////////////////////////////////
+const p1 = players('Human', 1);
+const p2 = players('Computer', 2);
+//////////////////////////////////
+// player picks p1(x) or p2(o)
+//
+//////////////////////////////////
 
-//////////////////////////////////////////////////////////////////////
-const game = (function() {  
-
+const game = (function() {
+  let currentPlayer = p1;
   const tileSelection = (event) => {
-    let tile = event.target.getAttribute('array-ref');
+    let tile = event.target.getAttribute('id');    
+    if (event.target.getAttribute('content') < 1) {    
+      gameBoard.renderMark(tile, currentPlayer.mark());
+      turnChange();
+      checkWin();
+    }
+  };
+  const turnChange = () => {
+    (currentPlayer == p1) ? currentPlayer = p2 : currentPlayer = p1;
+  };
+  
+  
+  
+  
+  
+  const checkWin = () => {    
+    let arrCopy = gameBoard.getBoard();
+    console.log(arrCopy)
     
-    gameBoard.renderMark(tile, ) //some variable for 1 or 2)    
-  }
+    for (i = 0; i < 3; i++) {
+     
+      let checkTotal = arrCopy[0][i] + arrCopy[1][i] + arrCopy[2][i];
+      let checkLine = false
+      // console.log(checkTotal)
+      // console.log(i)
+      // console.log(arrCopy[0][i])
+      // console.log(arrCopy[1][i])
+      // console.log(arrCopy[2][i])
+
+      if ((arrCopy[0][i] == arrCopy[1][i]) && (arrCopy[0][i] == arrCopy[2][i])) {
+        checkLine = true;
+        console.log (checkLine);
+        console.log (checkTotal)
+    }
+
+    // for (i = 0; i < 3; i++) {
+    //   if (arrCopy[i][0] == arrCopy[i][1] == arrCopy[i][2]) {
+    //     console.log("winner row");
+    //   }
+    // }
+    // for (i = 0; i < 3; i++) {
+    //   if (arrCopy[0][0] == arrCopy[1][1] == arrCopy[2][2]) {
+    //     console.log("winner diagonal1");
+    //   }
+    // }
+    // for (i = 0; i < 3; i++) {
+    //   if (arrCopy[0][2] == arrCopy[1][1] == arrCopy[2][0]) {
+    //     console.log("winner diagonal");
+    //   }
+    }
+
+  //   let arrCopy = [
+  //     [1, 0, 0],    
+  //     [0, 0, 0],
+  //     [0, 0, 0],
+  //   ];
+  
+  // undefined
+  // for (i = 0; i < arrCopy.length; i++) {
+  //     let checkLine = false;
+  //     if ((arrCopy[0][i] == arrCopy[1][i]) && (arrCopy[0][i] == arrCopy[2][i])) {
+  //         checkLine = true;
+  //     }
+  //     console.log(checkLine)
+  // }
+       
+  // VM1659:6 false
+  // 2VM1659:6 true
+    
+
+
+
+
+
+  };
+
+
+  //reset function has to clear all attribute 'content' from divs
 
   return {
-    tileSelection
+    tileSelection,    
   }
 
 })();
-
-/////////////////////////////////////////////////////////////////////
 
 gameBoard.renderBoard()
 
-
-
-// variable for game StaticRange, running, turn
-// event listeners 
-
-// 2 players, gameboard, game classes
-// in game have reference to players and game
-// have reference to state (running, results, over)
-// current player variable
-// function swtich player
-// event listeners
-// on click grab current player from game object 
-// assign to grid
-// check if complete
-// switch player if so
-
-// render function in game that calls gameboard.renderMark
